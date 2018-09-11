abe60436fdfcc478cc7e2266f7001ab2d5347c6c
shenyuan <775486362@qq.com>
2018-09-11 14:21:20 +0800

    if

---
+++ basicGrammar/IfDemo.java
@@ -0,0 +1,10 @@
+public class IfDemo{
+	public static void main(String[] args) {
+		int i = 5;
+		if(i == 5) {
+			System.out.println("if is true");
+			i++;
+		}
+		System.out.println(i);
+	}
+}
---
+++ basicGrammar/IfElseDemo.java
@@ -0,0 +1,11 @@
+
+public class IfElseDemo{
+    public static void main(String[] args) {
+        int i = 5;
+        if (i < 4) {
+            System.out.println("i < 4");
+        } else {
+            System.out.println("i >= 4");
+        }
+    }
+}

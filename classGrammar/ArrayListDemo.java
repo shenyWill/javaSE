27f50fb12ef45608aa75cded618cf54665505a17
shenyuan <775486362@qq.com>
2018-09-17 18:45:49 +0800

    array-list

--- classGrammar/ArrayListDemo.java
+++ classGrammar/ArrayListDemo.java
@@ -10,5 +10,23 @@ public class ArrayListDemo{
         String s = arrayList.get(1);
         System.out.println(size);
         System.out.println(s);
+
+        ArrayList<Integer> arrayList2 = new ArrayList<Integer>();
+        arrayList2.add(1);
+        arrayList2.add(2);
+        arrayList2.add(3);
+        arrayList2.add(4);
+        arrayList2.add(5);
+
+        arrayList2.add(2, 7);
+
+        arrayList2.set(0, 111);
+
+        arrayList2.remove(4);
+
+        arrayList2.clear();
+        for(int i=0; i < arrayList2.size(); i++) {
+            System.out.println(arrayList2.get(i));
+        }
     }
 }

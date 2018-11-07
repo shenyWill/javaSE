18ae46c5a9d4242d0832cdcd3df86c559ef0aef9
shenyuan <775486362@qq.com>
2018-11-07 15:45:59 +0800

    update select

--- mysql/select-student.sql
+++ mysql/select-student.sql
@@ -6,3 +6,17 @@ SELECT DISTINCT uname from student
 
 -- 查询重新命名列
 SELECT uname AS 'hahah' from student
+
+-- 查询时候直接进行计算
+SELECT uid+1000 FROM student
+
+-- 查询加条件
+SELECT * FROM student WHERE uid = 5
+SELECT * FROM student WHERE umoney > 6000
+SELECT * FROM student WHERE umoney > 6000 AND umoney < 10000
+
+-- between的条件查询
+SELECT * FROM student WHERE umoney BETWEEN 6000 AND 10000
+
+-- 查询金额是3500 4500 5500 其中一个
+SELECT * FROM student WHERE umoney in (3500,4500,5500)
